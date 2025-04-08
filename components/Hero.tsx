34ccc89dd704088cf23c87d5118cdb6a6bef9946
Mubarek <mubirimuru@gmail.com>
2025-04-09 02:23:42 +0300

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,9 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
+import { motion } from "motion/react";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-
+import Image from "next/image";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
@@ -39,6 +39,25 @@ const Hero = () => {
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
+      {
+        //hero image
+      }
+
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+        viewport={{ once: true }}
+        className="flex justify-center items-center"
+      >
+        <Image
+          src="/hero-img.png"
+          alt=""
+          className="rounded-full w-40 h-40"
+          width={50}
+          height={50}
+        />
+      </motion.div>
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
@@ -57,7 +76,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Mubarek, a Full Stack Developer.
           </p>
 
           <a href="#about">
